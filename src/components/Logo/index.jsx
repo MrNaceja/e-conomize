58bdbcdf5a09ab4c679a9b783c6d019e68aebb5d
Naceja <edutoriani13@gmail.com>
2023-02-15 21:43:33 -0300

    Criações

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,7 @@
+import './style.css'
+
+export default function Logo() {
+    return (
+        <h1 className="logo">e<span>$</span>nomize</h1>
+    )
+}
---
+++ src/pages/App/index.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+
+import Logo from '../../components/Logo'
+import Header from '../../components/Header'
+
+export default function App() {
+  return (
+    <div className="appContainer">
+      <Logo/>
+      <Header/>
+    </div>
+  )
+}
